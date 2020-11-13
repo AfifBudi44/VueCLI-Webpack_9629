7f9df32b4d2e4d777c708cbbf22814581ae2c994
afifbudi44 <afifutomo44@gmail.com>
2020-11-13 09:24:00 +0700

    Tugas VueCli-Webpack

--- src/components/TodoList/ListTugas.vue
+++ src/components/TodoList/ListTugas.vue
@@ -1,176 +1,345 @@
 <template>
     <v-main class="list">
-        <h3 class="text-h3 font-weight-medium mb-5">To Do List</h3>
+        <h3 class="text-h3 font-weight-medium mb-5">To Do List Tugas</h3>
 
         <v-card>
-            <v-card-title primary-title>
+            <v-card-title>
                 <v-text-field
-                v-model="search"
-                append-icon="mdi-magnify"
-                label="search"
-                single-line
-                hide-details></v-text-field>
-                <v-spacer></v-spacer>
-                <v-select
-                    :items="['All Priority','Penting', 'Biasa', 'Tidak Penting']"
-                    v-model="filters"
-                    label="Priority"
-                    @change="filterPriority"
-                    outlined
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Search"
+                    single-line
                     hide-details
-                    class="mr-4"
-                    dense></v-select>
-                <v-btn color="success" dark @click="dialog = true">Tambah</v-btn>
+                ></v-text-field>
+
+                <v-spacer></v-spacer>
+
+                <v-btn color="success" dark @click="dialog = true">
+                    tambah
+                </v-btn>
             </v-card-title>
-            
-            <v-data-table :headers="headers" :items="filterPriority" :search="search" pagination.sync="pagination">
-                <template v-slot:[`item.actions`]="{ item }">
-                    <v-icon small color="red" class="mr-2" @click="editItem(item)">
-                    mdi-pencil
-                    </v-icon>
-                    <v-icon color="blue" small @click="deleteItem(item)">
-                    mdi-delete
-                    </v-icon>
+
+            <v-data-table 
+                :headers="headers" 
+                :items="todos"
+                :search="search">
+
+                <template v-slot:[`item.priority`]="{ item }">
+                    <td>
+                        <v-card v-if="item.priority == 'Penting'" style="border-color: lightcoral; color: lightcoral; width: fit-content; padding: 2px 7px 2px 7px;" lined>
+                            {{ item.priority }}
+                        </v-card>
+                        <v-card v-else-if="item.priority == 'Biasa'" style="border-color: lightblue; color: lightblue; width: fit-content; padding: 2px 7px 2px 7px;" lined>
+                            {{ item.priority }}
+                        </v-card>
+                        <v-card v-else lined style="border-color: lightgreen; color: lightgreen; width: fit-content; padding: 2px 7px 2px 7px;" lined>
+                            {{ item.priority }}
+                        </v-card>
+                    </td>
                 </template>
+
+                <template v-slot:[`item.actions`]="{ item }">
+
+                    <v-icon small class="icnote mr-2" @click="detailItem(item)">{{ icons.mdiTextBoxSearchOutline}}</v-icon>
+                    <v-icon small class="pencil mr-2" @click="editItem(item)">{{ icons.mdiPencil }}</v-icon>
+                    <v-icon small class="bin mr-2" @click="deleteItem(item)"> {{ icons.mdiDelete }}</v-icon>
+                </template>
+
+                <template v-slot:[`item.select`]="{ item }">
+                    <v-checkbox label="Click here to select delete"  small class="checkbox" @click="toggleSelect(item)"> {{ icons.mdiDelete }}</v-checkbox>
+                </template>
+
             </v-data-table>
         </v-card>
 
-        <v-dialog
-            v-model="dialog"
-            persistent
-            max-width="600px"
-            transition="dialog-transition"
-        >
+        <v-card v-if="selected.length" style="margin-top: 30px; padding: 20px;">
+            <v-card-title>
+                <h4>Delete Multiple:</h4>
+            </v-card-title>
+            <v-list-item
+                v-for="(item, i) in selected"
+                :key="i">
+                <v-list-item-content>
+                    <v-list-item-title>•  {{item.task}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-btn color="red lighten-2" dark @click="deleteSelected">
+                hapus semua
+            </v-btn>
+        </v-card>
+
+        <v-dialog v-model="dialog" persistent max-width="600px">
             <v-card>
                 <v-card-title>
-                    <span class="headline">From Todo</span>
+                    <span class="headline" 
+                        v-if="adding == true" >
+                           Form Todo - Add
+                    </span>
+                    <span class="headline" 
+                        v-else>
+                           Form Todo - Edit
+                    </span>
                 </v-card-title>
+
                 <v-card-text>
                     <v-container>
                         <v-text-field
                             v-model="formTodo.task"
                             label="Task"
                             required
+                            autofocus
                         ></v-text-field>
+
                         <v-select
-                            :items="['Penting', 'Biasa', 'Tidak Penting']"
                             v-model="formTodo.priority"
-                            label="All Priority"
+                            :items="['Penting', 'Biasa', 'Tidak penting']"
+                            label="Priority"
                             required
                         ></v-select>
+
                         <v-textarea
-                        v-model="formTodo.note" label="Note" required>
-                        </v-textarea>
+                            v-model="formTodo.note"
+                            label="Note"
+                            required
+                        ></v-textarea>
                     </v-container>
                 </v-card-text>
+
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="cancel">Cancel</v-btn>
-                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                    
+                    <v-btn color="blue darken-1" text @click="cancel">
+                        Cancel
+                    </v-btn>
+
+                    <v-btn v-if="adding == true" 
+                        color="blue darken-1" 
+                        text 
+                        @click="save">
+                        Save
+                    </v-btn>
+
+                    <v-btn v-else 
+                        color="blue darken-1" 
+                        text 
+                        @click="edit(formTodo)">
+                        Save
+                    </v-btn>
+
                 </v-card-actions>
             </v-card>
         </v-dialog>
+
+                    
+        <v-dialog v-model="dialogdel" persistent max-width="400px">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">
+                        Yakin ingin menghapus?
+                    </span>
+                </v-card-title>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    
+                    <v-btn color="green darken-1" text @click="cancel">
+                        Tidak
+                    </v-btn>
+
+                    <v-btn color="red darken-1" text @click="confirmdelete">
+                        Ya
+                    </v-btn>
+
+                </v-card-actions>
+
+            </v-card>
+        </v-dialog>
+
+                    
+        <v-dialog v-model="dialognote" persistent max-width="400px">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">
+                        {{detail.task}}
+                    </span>
+                </v-card-title>
+                
+                <v-card-text>
+                    <v-container>
+                        <v-card v-if="detail.priority == 'Penting'" style="border-color: lightcolar; color: lightcolar; width: fit-content; padding: 2px 7px 2px 7px;" lined>
+                            {{detail.priority}}
+                        </v-card>
+                        <v-card v-else-if="detail.priority == 'Biasa'" style="border-color: lightblue; color: lightblue; width: fit-content; padding: 2px 7px 2px 7px;" lined>
+                            {{detail.priority}}
+                        </v-card>
+                        <v-card v-else lined style="border-color: lightgreen; color: lightgreen; width: fit-content; padding: 2px 7px 2px 7px;">
+                            {{detail.priority}}
+                        </v-card>
+                        <br>
+                        {{detail.note}}
+                    </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="red darken-1" text @click="cancel">
+                        Close
+                    </v-btn>
+                </v-card-actions>
+
+            </v-card>
+        </v-dialog>
+
     </v-main>
 </template>
 <script>
+import {
+    mdiPencil,
+    mdiDelete,
+    mdiTextBoxSearchOutline,
+} from '@mdi/js'
 export default {
-    name:"List",
-    data(){
+    name: "List",
+    data() {
         return {
-            search:null,
-            dialog:false,
-            tempTodo:null,
-            filters:"All Priority",
+            search: null,
+            adding: true,
+            edititem: null,
+            dialog: false,
+            dialogdel: false,
+            dialognote: false,
             selected: [],
+    
+            icons: {
+                mdiPencil,
+                mdiDelete,
+                mdiTextBoxSearchOutline,
+            },
+            filters: {
+                search: '',
+                priority: '',
+            },
             headers: [
                 {
-                    text:'Task',
-                    align: 'start',
+                    text: "Task",
+                    align: "start",
                     sortable: true,
-                    value: 'task',
+                    value: "task",
                 },
-                { text: "Priority", value:"priority"},
-                { text: "Note", value:"note"},
-                { text: "Actions", value:"actions"},
+                
+                { 
+                    text: "Priority",
+                    field: "priority", 
+                    value: "priority" 
+                },
+                { 
+                    text: "Actions", 
+                    value: "actions", 
+                    sortable: false,
+                },
+                {
+                    text: "Select to Delete",
+                    value: "select",
+                    sortable: false,
+                }
             ],
             todos: [
                 {
                     task: "bernafas",
                     priority: "Penting",
-                    note: "hufftts",
+                    note: "huffttt",
                 },
                 {
                     task: "nongkrong",
-                    priority: "Tidak Penting",
-                    note: "bersama teman teman"
+                    priority: "Tidak penting",
+                    note: "bersama tman2",
+                },
+                {
+                    task: "masak",
+                    priority: "Biasa",
+                    note: "masak air 500ml",
                 },
             ],
-            formTodo:  {
+            formTodo: {
                 task: null,
                 priority: null,
                 note: null,
             },
+            detail: {
+                task: null,
+                priority: null,
+                note: null,
+            }
         };
     },
     methods: {
-        save(){
-            let index =  this.findIndexTodos(this.formTodo);
-            if(index < 0){ 
-                this.todos.push(this.formTodo);
-            }else {
-                this.todos[index] = this.formTodo;
-            }
+        save() {
+            this.todos.push(this.formTodo);
+            this.cancel();
+        },
+        cancel() {
             this.resetForm();
             this.dialog = false;
+            this.edititem = null;
+            this.adding = true;
+            this.dialogdel = false;
+            this.dialognote = false;
         },
-        cancel(){
-            let index =  this.findIndexTodos(this.formTodo);
-            if(index < 0){ 
-                this.todos.push(this.tempTodo);
-            }else {
-                this.todos[index] = this.tempTodo;
+        deleteItem(item) {
+            this.dialogdel = true;
+            this.edititem = item;
+        },
+        confirmdelete() {
+            this.todos.splice(this.todos.indexOf(this.edititem), 1);
+            this.dialogdel = false;
+        },
+        editItem(item) {
+            this.adding = false;
+            this.formTodo = {
+                task: item.task,
+                priority: item.priority,
+                note: item.note,
+            };
+            this.dialog = true;
+            this.edititem = item;
+        },
+        detailItem(item) {
+            this.detail = {
+                task: item.task,
+                priority: item.priority,
+                note: item.note,
             }
-            console.log(this.tempTodo);
-            this.resetForm();
-            this.dialog = false;
+            this.dialognote = true;
         },
-        resetForm(){
+        edit(formTodo) {
+            this.edititem.task = formTodo.task;
+            this.edititem.priority = formTodo.priority;
+            this.edititem.note = formTodo.note;
+            this.cancel();
+        },
+        
+        resetForm() {
             this.formTodo = {
                 task: null,
                 priority: null,
                 note: null,
             };
         },
-        editItem(item){
-            this.formTodo = item;
-            this.dialog = true;
-        },
-        deleteItem(item){
-            let x = window.confirm("Apa yakin ingin menghapus?  ");
-            if(x === true){
-                let index = this.findIndexTodos(item);
-                this.todos.splice(index,1);
+        toggleSelect(item) {
+            if(this.selected.includes(item)) {
+                this.selected.splice(this.selected.indexOf(item), 1);
+            } else {
+                this.selected.push(item);
             }
         },
-        findIndexTodos(item){
-            return this.todos.findIndex(obj => obj.task === item.task);
+        deleteSelected () {
+            for(var i = 0; i < this.selected.length; i++){
+                const index = this.todos.indexOf(this.selected[i]);
+                this.todos.splice(index, 1);
+            }
+            this.selected=[];
+            toggleSelect(this.todos);
         },
-        
     },
-    computed: {
-        filterPriority(){
-            let finds = this.filters;
-            if(finds == "All Priority"){
-                return this.todos;
-            }else {
-                var fil = this.todos.filter(function(x){
-                    return x.priority == finds;
-                })
-                return fil;
-            }
-            
-        },
-    }
-    
 };
 </script>
